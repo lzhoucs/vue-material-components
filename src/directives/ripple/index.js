5cb1f6bc05867d1e00fd5e03aade77463fd0d45b
Liang Zhou <lzhoucs@gmail.com>
2020-04-07 00:03:59 -0500

    wip2.

--- src/directives/ripple/index.js
+++ src/directives/ripple/index.js
@@ -1,4 +1,5 @@
-
+import {numbers, strings, cssClasses} from '@material/ripple/constants'
+import {getNormalizedEventCoords} from '@material/ripple/util';
 
 const ACTIVATION_EVENT_TYPES = [
   // only mousedown for now
@@ -8,35 +9,58 @@ const ACTIVATION_EVENT_TYPES = [
   // 'keydown'
 ];
 
-const activate = evt => {
-  console.log('clicked', evt)
+const getWindowPageOffset = () => ({x: window.pageXOffset, y: window.pageYOffset})
 
-  // layout
-  const {
-    width,
-    height
-    // x
-    // y
-    // top
-    // right
-    // bottom
-    // left
-  } = el.getBoundingClientRect();
+  const getFgTranslationCoordinates = () => {
+
+    return {startPoint, endPoint};
+  }
+const activate = evt => {
+
+  const el = evt.target
+
+  // layoutInternal
+  const { width, height } = el.getBoundingClientRect();
   const maxDim = Math.max(width, height);
-  const hypotenuse = Math.sqrt(Math.pow(_this.frame_.width, 2) + Math.pow(_this.frame_.height, 2));
+  const hypotenuse = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
   const maxRadius = hypotenuse + MDCRippleFoundation.numbers.PADDING;
-        // Ripple is sized as a fraction of the largest dimension of the surface, then scales up using a CSS scale transform
-        var initialSize = Math.floor(maxDim * MDCRippleFoundation.numbers.INITIAL_ORIGIN_SCALE);
-        // Unbounded ripple size should always be even number to equally center align.
-        if (this.adapter_.isUnbounded() && initialSize % 2 !== 0) {
-            this.initialSize_ = initialSize - 1;
-        }
-        else {
-            this.initialSize_ = initialSize;
-        }
-        this.fgScale_ = "" + this.maxRadius_ / this.initialSize_;
-        this.updateLayoutCssVars_();
+  const initialSize = Math.floor(maxDim * numbers.INITIAL_ORIGIN_SCALE);
+
+    const {
+      VAR_FG_SIZE, VAR_FG_SCALE
+    } = strings;
+
+  el.style.setProperty(VAR_FG_SIZE, `${initialSize_}px`);
+  el.style.setProperty(VAR_FG_SCALE, `${maxRadius / initialSize}`);
+
+
+  // animateActivation
+    const {VAR_FG_TRANSLATE_START, VAR_FG_TRANSLATE_END} = strings;
+    const {FG_DEACTIVATION, FG_ACTIVATION} = cssClasses;
+    const {DEACTIVATION_TIMEOUT_MS} = numbers;
+
+
+  let startPoint = getNormalizedEventCoords(evt, getWindowPageOffset(), el.getBoundingClientRect());
+
+  startPoint = {
+      x: startPoint.x - (initialSize / 2),
+      y: startPoint.y - (initialSize / 2),
+    };
+
+    const endPoint = {
+      x: (width / 2) - (initialSize / 2),
+      y: (height / 2) - (initialSize / 2),
+    };
+      const translateStart = `${startPoint.x}px, ${startPoint.y}px`;
+      const translateEnd = `${endPoint.x}px, ${endPoint.y}px`;
+
+    el.style.setProperty(VAR_FG_TRANSLATE_START, translateStart);
+    el.style.setProperty(VAR_FG_TRANSLATE_END, translateEnd);
+
+//     this.adapter_.addClass(FG_ACTIVATION);
+//     this.activationTimer_ = setTimeout(() => this.activationTimerCallback_(), DEACTIVATION_TIMEOUT_MS);
 }
+
 export default {
     // directive definition
     beforeMount: function (el) {
