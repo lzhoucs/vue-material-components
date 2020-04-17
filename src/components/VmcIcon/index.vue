c52b33fe6737ab4691e8c1edece981135ff148a3
Liang Zhou <lzhoucs@gmail.com>
2020-04-17 10:12:03 -0500

    Button with Icon support.

---
+++ src/components/VmcIcon/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <i class="material-icons mdc-button__icon" aria-hidden="true">
+    <slot></slot>
+  </i>
+</template>
+
+<script>
+export default {
+  name: "VmcIcon"
+}
+</script>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export {default as ripple} from "./directives/ripple"
 export {default as VmcButton} from "./components/VmcButton/index.vue"
+export {default as VmcIcon} from "./components/VmcIcon/index.vue"
