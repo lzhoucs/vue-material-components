be87c20956db380d71803839f454d22056141f73
Liang Zhou <lzhoucs@gmail.com>
2020-04-17 13:29:56 -0500

    Rest of button style support.

--- src/components/VmcButton/index.vue
+++ src/components/VmcButton/index.vue
@@ -1,6 +1,10 @@
 <template>
   <button class="mdc-button"
-    :class="raised && 'mdc-button--raised'"
+    :class="[
+      raised && 'mdc-button--raised',
+      unelevated && 'mdc-button--unelevated',
+      outlined && 'mdc-button--outlined'
+    ]"
     v-ripple>
    <div class="mdc-button__ripple"></div>
    <span class="mdc-button__label"><slot></slot></span>
@@ -14,6 +18,14 @@ export default {
   name: 'vpc-button',
   props: {
     raised: {
+      type: Boolean,
+      default: false
+    },
+    unelevated: {
+      type: Boolean,
+      default: false
+    },
+    outlined: {
       type: Boolean,
       default: false
     }
