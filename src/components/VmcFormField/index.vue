fab2e6d327f2ca86f6b30121a4b6ac69e58664cc
Liang Zhou <lzhoucs@gmail.com>
2020-04-27 18:19:58 -0500

    Export VmcFormField.

--- src/components/VmcFormField/index.vue
+++ src/components/VmcFormField/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="mdc-form-field">
     <template v-if="label">
+      <!-- this results in the following bad verbose usage. It would be really nice if there's a way to touch the slot root element directly
+      <vmc-form-field label="with explicit form field">
+        <template v-slot:default="{id}">
+          <vmc-checkbox :id="id"></vmc-checkbox>
+        </template>
+      </vmc-form-field>
+      -->
+
       <slot :id="instanceId"></slot>
       <label :for="instanceId">{{label}}</label>
     </template>
